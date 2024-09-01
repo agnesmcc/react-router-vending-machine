1ee206a2678ce51e547ad391794fae4927b7b5a4
Agnes <agnes.mccall5@gmail.com>
2024-08-31 21:56:07 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import VendingMachine from './VendingMachine';
 import Soda from './Soda';
 import Chips from './Chips';
 import IceCream from './IceCream';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import './navbar.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <div className='navbar'>
+          <NavLink to="/soda">Soda</NavLink>
+          <NavLink to="/chips">Chips</NavLink>
+          <NavLink to="/icecream">Ice Cream</NavLink>
+        </div>
         <Routes>
           <Route path="/" element={<VendingMachine />} />
           <Route path="/soda" element={<Soda />} />
